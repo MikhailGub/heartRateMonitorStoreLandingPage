b3ab202c6e1b453ba7ff8fe613bca992c24fe5da
Mikhail <mgubankov@yandex.ru>
2023-09-13 09:09:29 +0500

    added the pageup link (not finished), shadows on buttons, mailer, validation, phone mask, updated js code, changed .gitignore

--- src/js/script.js
+++ src/js/script.js
@@ -61,4 +61,52 @@ $(document).ready(function(){
            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
         }
       });
+
+
+    function valideteForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите своё имя",
+                phone: "Пожалуйста, введите свой номер",
+                email: {
+                    required: "Пожалуйста, введите свой e-mail",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+    valideteForms('#consultation-form');
+    valideteForms('#consultation form');
+    valideteForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset')
+        });
+        return false;
+    });
 });
